21741fb197534dc069fbcb82f4903df824267e64
Taylor <taylorpollioverturf@gmail.com>
2021-10-11 18:47:58 -0700

    end of day saving of work

--- portfolio-script.js
+++ portfolio-script.js
@@ -1,5 +1,15 @@
 // This function is to toggle the hamburger dropdown menu when clicked. 
-$('.icon').on('click', function(event){
-  event.preventDefault();
-  $('.topnav #myLinks').toggle();
-});
+    $('.icon').on('click', function(event){
+      event.preventDefault();
+      $('.topnav #myLinks').toggle();
+    });
+
+// I want the welcome text to bounce! 
+    $('.fa-html5').on('click', function(event){
+      $('.fa-html5').effect( 'bounce', {times:3}, 'slow' );
+    });
+
+  //   $('.fa-html5').on("click", function () {
+  //     action("bounce");
+  //     return false;
+  // });
