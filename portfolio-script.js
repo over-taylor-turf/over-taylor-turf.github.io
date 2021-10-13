12f888ff114c476368163d471f94adf7ad55db05
Taylor <taylorpollioverturf@gmail.com>
2021-10-13 15:00:30 -0700

    phase one of slidshow, bugs need fixing

--- portfolio-script.js
+++ portfolio-script.js
@@ -67,3 +67,37 @@
       $('#secret').on('click', function(event){
         $('#secret').effect('shake', {times: 2, distance: 10}, 450);
       });
+    
+
+      // TESTIMONIALS 
+
+      // practice with fades 
+
+      // $('#testimonial1').on('click', function(event){
+      //   $('#testimonial1').fadeToggle('slow');
+      // });
+
+      
+      // $("button").click(function(){
+      //   $("p").hide(1000);
+      // });
+
+      // $('#testimonial1').on('click', function(event){
+      //   $('#testimonial1').hide(1000);
+      // });
+      
+      // $('#testimonial1').on('click', function(event){
+      //   $('#testimonial1').fadeOut('slow');
+      // });
+
+
+      $("#slideshow > div:gt(0)").hide();
+
+            setInterval(function() {
+              $('#slideshow > div:first')
+                .fadeOut(1000)
+                .next()
+                .fadeIn(1000)
+                .end()
+                .appendTo('#slideshow');
+            }, 3000);
